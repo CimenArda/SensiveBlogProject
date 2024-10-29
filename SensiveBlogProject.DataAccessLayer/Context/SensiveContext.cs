b1ef890d5fd52785dd67bfc7fc4322ee70fbf2e1
ArdaCimen <cimenarda.yaz@gmail.com>
2024-10-29 16:08:50 +0300

    Diğer Katmanların Oluşturulması Ve Bazı İşlemler

---
+++ SensiveBlogProject.DataAccessLayer/Context/SensiveContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SensiveBlogProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensiveBlogProject.DataAccessLayer.Context
+{
+    public class SensiveContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=ARDACIMEN\\SQLEXPRESS;initial Catalog=SensiveBlog;integrated Security=true;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<NewsLetter> NewsLetters { get; set; }
+        public DbSet<TagCloud> TagClouds { get; set; }
+
+    }
+}
